e3a7531846e66ae6eb8a40876e30f04901816724
unknown <rafaeluissantos1@gmail.com>
2019-05-05 15:50:08 -0300

    Alterando posicionamento da camera

--- src/mygame/Main.java
+++ src/mygame/Main.java
@@ -1,18 +1,29 @@
 package mygame;
 
 import com.jme3.app.SimpleApplication;
+import com.jme3.light.DirectionalLight;
 import com.jme3.material.Material;
 import com.jme3.math.ColorRGBA;
+import com.jme3.math.FastMath;
+import com.jme3.math.Vector3f;
 import com.jme3.renderer.RenderManager;
+import com.jme3.scene.CameraNode;
 import com.jme3.scene.Geometry;
+import com.jme3.scene.Spatial;
+import com.jme3.scene.control.CameraControl.ControlDirection;
 import com.jme3.scene.shape.Box;
 
 /**
  * This is the Main Class of your Game. You should only do initialization here.
  * Move your Logic into AppStates or Controls
+ *
  * @author normenhansen
  */
 public class Main extends SimpleApplication {
+
+    private Spatial tank;
+    private CameraNode camNode;
+    private Vector3f posinit;
 
     public static void main(String[] args) {
         Main app = new Main();
@@ -22,26 +33,59 @@ public class Main extends SimpleApplication {
 
     @Override
     public void simpleInitApp() {
-        
+
         flyCam.setMoveSpeed(75);
+
+        /**
+         * A white, directional light source
+         */
+        DirectionalLight sun = new DirectionalLight();
+        sun.setDirection((new Vector3f(-0.5f, -0.5f, -0.5f)).normalizeLocal());
+        sun.setColor(ColorRGBA.White);
+        rootNode.addLight(sun);
+
+        tank = createTank("MyTank");
+        rootNode.attachChild(tank);
         
-        Box b = new Box(1, 1, 1);
-        Geometry geom = new Geometry("Box", b);
-
-        Material mat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
-        mat.setColor("Color", ColorRGBA.Blue);
-        geom.setMaterial(mat);
-
-        rootNode.attachChild(geom);
+        camNode = new CameraNode("CamNode", cam);
+        //camNode.setControlDir(ControlDirection.SpatialToCamera);
+        rootNode.attachChild(camNode);  
+        
+        
+        
     }
 
     @Override
     public void simpleUpdate(float tpf) {
         //TODO: add update code
+        tank.move(0,0,tpf);
+        Vector3f pos = tank.getLocalTranslation().clone();
+        
+        posinit = tank.getLocalTranslation().clone();
+        posinit.y +=10;
+        //posinit. +=5;
+        
+        pos.z -= 5;
+        pos.y += 25;
+       
+        
+        camNode.setLocalTranslation(pos);
+        camNode.lookAt(posinit, Vector3f.UNIT_Z);
+        //camNode.lookAt(posinit, Vector3f.UNIT_Z);
     }
 
     @Override
     public void simpleRender(RenderManager rm) {
         //TODO: add render code
+    }
+
+    public Spatial createTank(String name) {
+        /**
+         * Load a model. Uses model and texture from jme3-test-data library!
+         */
+        Spatial tank = assetManager.loadModel("Models/Tank/tank.j3o");
+        tank.scale(0.35f);
+
+        return tank;
     }
 }
