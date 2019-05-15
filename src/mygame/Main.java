a9798186401a19e54f4e5b7d573e3b592770dce4
unknown <rafaeluissantos1@gmail.com>
2019-05-14 22:22:03 -0300

    Movimentos do teclado

--- src/mygame/Main.java
+++ src/mygame/Main.java
@@ -1,8 +1,15 @@
 package mygame;
 
+import com.jme3.animation.AnimChannel;
+import com.jme3.animation.AnimControl;
+import com.jme3.animation.AnimEventListener;
 import com.jme3.app.SimpleApplication;
 import com.jme3.collision.CollisionResult;
 import com.jme3.collision.CollisionResults;
+import com.jme3.input.KeyInput;
+import com.jme3.input.controls.ActionListener;
+import com.jme3.input.controls.AnalogListener;
+import com.jme3.input.controls.KeyTrigger;
 import com.jme3.light.DirectionalLight;
 import com.jme3.material.Material;
 import com.jme3.math.ColorRGBA;
@@ -19,15 +26,14 @@ import com.jme3.scene.shape.Box;
 import java.util.ArrayList;
 import java.util.Random;
 
-
 /**
  * This is the Main Class of your Game. You should only do initialization here.
  * Move your Logic into AppStates or Controls
  *
  * @author normenhansen
  */
-public class Main extends SimpleApplication {
-    
+public class Main extends SimpleApplication implements AnimEventListener {
+
     private CameraNode camNode;
     private Vector3f posinit;
     private Spatial tank;
@@ -35,9 +41,7 @@ public class Main extends SimpleApplication {
     private long delayEnemy;
     private ArrayList<Enemy> enemyList = new ArrayList<Enemy>();
     Random rand = new Random();
-    
-    
-    
+
     public static void main(String[] args) {
         Main app = new Main();
         app.setShowSettings(false);
@@ -57,55 +61,48 @@ public class Main extends SimpleApplication {
         sun.setColor(ColorRGBA.White);
         rootNode.addLight(sun);
 
-        
+        initKeys();
+
         tank = createTank("MyTank");
         rootNode.attachChild(tank);
-        
+
         camNode = new CameraNode("CamNode", cam);
         //camNode.setControlDir(ControlDirection.SpatialToCamera);
-        rootNode.attachChild(camNode);  
-        
+        rootNode.attachChild(camNode);
+
         Vector3f pos = tank.getLocalTranslation().clone();
-        
+
         posinit = tank.getLocalTranslation().clone();
-        posinit.y +=40;
-        
-        
+        posinit.y += 40;
+
         pos.z -= 3;
         pos.y += 50;
-       
-        
+
         camNode.setLocalTranslation(pos);
         camNode.lookAt(posinit, Vector3f.UNIT_Z);
-        
-        
+
     }
 
     @Override
     public void simpleUpdate(float tpf) {
-        
+
         //TODO: add update code
         //tank.move(0,0,tpf);
-        
         //camNode.lookAt(posinit, Vector3f.UNIT_Z);
-        
         long time = System.currentTimeMillis();
-        
-    
-        if(time > delayEnemy+3000)
-        {
-            boolean hasCollision = true;
-            
-            Enemy enemy = createEnemy(time);
-            do
-            {
-                hasCollision = hasCollision(enemy);
-                if(hasCollision)
-                    enemy.getSpatial().setLocalTranslation(((rand.nextFloat() * 70) - 40), -3, 30);
-                
-            }while(hasCollision);
 
-            
+        if (time > delayEnemy + 3000) {
+            boolean hasCollision = true;
+
+            Enemy enemy = createEnemy(time);
+            do {
+                hasCollision = hasCollision(enemy);
+                if (hasCollision) {
+                    enemy.getSpatial().setLocalTranslation(((rand.nextFloat() * 70) - 40), -3, 30);
+                }
+
+            } while (hasCollision);
+
             /*
             CollisionResults results = new CollisionResults();
             if(!enemyList.isEmpty())
@@ -130,16 +127,12 @@ public class Main extends SimpleApplication {
             }  
             else
                 enemyList.add(enemy);
-            */
+             */
             enemyList.add(enemy);
             rootNode.attachChild(enemy.getSpatial());
-           
+
         }
-      
-                        
-                        
-            
-        
+
     }
 
     @Override
@@ -157,39 +150,98 @@ public class Main extends SimpleApplication {
 
         return tank;
     }
-    
-    public Enemy createEnemy(long timeEnemy)
-    {
+
+    public Enemy createEnemy(long timeEnemy) {
         delayEnemy = timeEnemy;
-        
-        
+
         Enemy enemy = new Enemy();
         Spatial s = assetManager.loadModel("Models/Tank/tank.j3o");
         s.scale(0.35f);
-        
+
         s.setLocalTranslation(((rand.nextFloat() * 70) - 40), -3, 30);
         s.rotate(0, FastMath.PI, 0);
         s.setName(Integer.toString(cont));
         cont++;
-        
+
         enemy.setSpatial(s);
         enemy.setName(s.getName());
-        
-        
-        
+
         return enemy;
     }
-    
-    public boolean hasCollision(Enemy enemy)
-    {
+
+    public boolean hasCollision(Enemy enemy) {
         CollisionResults results = new CollisionResults();
-        for(Enemy e: enemyList)
-        {
+        for (Enemy e : enemyList) {
             enemy.getSpatial().collideWith(e.getSpatial().getWorldBound(), results);
-            
-            if (results.size() > 0)
+
+            if (results.size() > 0) {
                 return true;
+            }
         }
         return false;
     }
+
+    @Override
+    public void onAnimCycleDone(AnimControl control, AnimChannel channel, String animName) {
+        //implementar
+    }
+
+    public void onAnimChange(AnimControl control, AnimChannel channel, String animName) {
+        //unused
+    }
+
+    private void initKeys() {
+        inputManager.addMapping("Shoot", new KeyTrigger(KeyInput.KEY_SPACE));
+        inputManager.addMapping("Front", new KeyTrigger(KeyInput.KEY_W));
+        inputManager.addMapping("Back", new KeyTrigger(KeyInput.KEY_S));
+        inputManager.addMapping("Right", new KeyTrigger(KeyInput.KEY_D));
+        inputManager.addMapping("Left", new KeyTrigger(KeyInput.KEY_A));
+        inputManager.addListener(actionListener, "Shoot");
+        inputManager.addListener(analogListener, "Front");
+        inputManager.addListener(analogListener, "Back");
+        inputManager.addListener(analogListener, "Right");
+        inputManager.addListener(analogListener, "Left");
+    }
+
+    /**
+     * Use ActionListener to respond to pressed/released inputs (key presses,
+     * mouse clicks)
+     */
+    private ActionListener actionListener = new ActionListener() {
+
+        public void onAction(String name, boolean keyPressed, float tpf) {
+            if (name.equals("Shoot") && !keyPressed) {
+                System.out.println("Atirou");
+            }
+            
+            
+        }
+    };
+    
+    private AnalogListener analogListener = new AnalogListener() {
+        
+        public void onAnalog(String name, float value, float tpf) {
+            
+            if(name.equals("Front"))
+            {   
+                tank.move(0, 0, 0.02f);
+            }
+            
+            if(name.equals("Back"))
+            {   
+                tank.move(0, 0, -0.02f);
+            }
+            
+            if(name.equals("Right"))
+            {   
+                tank.move(-0.02f, 0, 0);
+            }
+            
+            if(name.equals("Left"))
+            {   
+                tank.move(0.02f, 0, 0);
+            }
+        }
+    };
+
 }
